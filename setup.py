7fdc8258b58df0f3827da4668bb17db6ebbcd4ca
Aizaz Iqbal <aizaziqbal11@gmail.com>
2025-11-17 21:24:26 +0500

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_pakages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aizaz Iqbal",
+    author_email="aizaziqbal11@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
